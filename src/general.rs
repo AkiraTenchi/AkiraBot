1359c6a47fe1c11bf71d47298b2f182128a19c59
AkiraTenchi <34791734+AkiraTenchi@users.noreply.github.com>
2021-10-06 16:25:23 +0200

    base structure of bot created

---
+++ src/general.rs
@@ -0,0 +1,19 @@
+use serenity::{
+    async_trait,
+    client::{Client, Context, EventHandler},
+    framework::standard::{
+        macros::{command, group},
+        CommandResult, StandardFramework,
+    },
+    model::{channel::Message, gateway::Ready},
+};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,41 @@
+mod general;
+use general::GENERAL_GROUP;
+use serenity::{
+    async_trait,
+    client::{Client, Context, EventHandler},
+    framework::standard::{
+        macros::{command, group},
+        CommandResult, StandardFramework,
+    },
+    model::{channel::Message, gateway::Ready},
+};
 use std::fs;
 
-fn main() {
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("*")) // set prefix to '*'
+        .group(&GENERAL_GROUP);
+
+    // login via token from file
     let token = fs::read_to_string("token.txt").expect("token from file");
-    println!("{}", token);
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error while creating client");
+
+    // start listening to event
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 }
